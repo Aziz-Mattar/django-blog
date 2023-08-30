d5f39437078b730cb4999bae0e665252e34174fb
aziz-mattar <aziz.k.mattar@gmail.com>
2023-08-30 11:44:30 +0300

    Fixing the Blog

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-
 from .models import Post, Category
 
-admin.site.register([Post, Category])
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInLine,
+    ]
