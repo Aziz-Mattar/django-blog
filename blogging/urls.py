af8f068e49d7ae31ce74c43dcb2f1409e0936966
Aziz-Mattar <aziz.k.mattar@gmail.com>
2023-09-04 11:24:58 -0800

    class based views

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import stub_view, list_view, detail_view
+from .views import BlogListView, BlogDetailView
 urlpatterns = [
-    path('', list_view, name='blog_index'),
-    path('posts/<int:post_id>', detail_view, name='blog_detail'),
+    path('', BlogListView.as_view(), name='blog_index'),
+    path('posts/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
 ]
