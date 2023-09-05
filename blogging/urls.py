d2d8ace8e618b96a0868752cf300804baacef1ad
Aziz-Mattar <aziz.k.mattar@gmail.com>
2023-09-05 07:43:34 -0800

    edit urls file

--- blogging/urls.py
+++ blogging/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import BlogListView, BlogDetailView
 urlpatterns = [
     path('', BlogListView.as_view(), name='blog_index'),
-    path('posts/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
+    path('posts/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
 ]
