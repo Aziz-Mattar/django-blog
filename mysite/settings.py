b7ed0e277af032808601ff0de2137c0889347f92
Aziz-Mattar <aziz.k.mattar@gmail.com>
2023-09-05 10:38:35 -0800

    requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ LOGIN_REDIRECT_URL = "/"
 
 AUTHENTICATION_BACKENDS = (
     # "django.contrib.auth.backends.ModelBackend",
-    # "allauth.account.auth_backends.AuthenticationBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 )
 
 SITE_ID = 1
